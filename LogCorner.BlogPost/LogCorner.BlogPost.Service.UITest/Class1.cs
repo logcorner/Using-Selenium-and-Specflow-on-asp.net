5ae926ebc50975f55e6975f11807caf5c794ed17
TOCANE TECHNOLOGIES <tocane.technologies@gmail.com>
2018-01-20 18:40:00 +0100

    test

---
+++ LogCorner.BlogPost/LogCorner.BlogPost.Service.UITest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogCorner.BlogPost.Web.Mvc.UITest
+{
+    [TestFixture]
+    class Class1
+    {
+[Test]
+        public void test ()
+        {
+            Assert.True(true);
+        }
+    }
+}
